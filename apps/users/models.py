2279a42dcffaead811dac605e91dc2051dc49fac
Elvin Zeng <zenghui@188.com>
2017-05-29 13:04:20 +0800

    modify the user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,12 +3,15 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 __author__ = "Elvin Zeng"
 
 
 class UserProfile(AbstractUser):
+    is_hr = models.BooleanField(default=False, null=False, verbose_name=u'帐号是否属于HR')
+
     class Meta:
         verbose_name = u"用户信息"
         verbose_name_plural = verbose_name
